7e3b72444c552537e62f425e0dfd7cea530d1031
GizIOn <34921969+GizIOn@users.noreply.github.com>
2022-05-05 00:55:35 +0500

    got first pages in needed pdf how to rescale

--- remake_pdfs.py
+++ remake_pdfs.py
@@ -10,14 +10,16 @@ for dirpath, _, filenames in os.walk(your_target_folder):
             pdf_files.append(file_full_path)
 pdf_files.sort(key=str.lower)
 
-writer = PdfFileWriter()
 
 for file_path in pdf_files:
-
+    writer = PdfFileWriter()
     reader = PdfFileReader(file_path)
     page = reader.getPage(0)
-    page.scaleBy(0.1)
+    var = page.mediaBox
+    page.mediaBox.upperLeft=(0,15)
+    page.mediaBox.upperRight=(0,15)
+    #page.scaleBy(0.1)
     writer.addPage(page)
 
-with open("CombinedFirstPages.pdf", "wb") as output:
-    writer.write(output)
+    with open(f"{file_path}", "wb") as output:
+        writer.write(output)
