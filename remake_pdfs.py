7bdcfeefbe4589606b1b8146b14fcdb6b5e32d9c
Geara0 <adrezz.26.5@gmail.com>
2022-05-20 20:31:26 +0500

    Нажал ctrl+alt+l за ильгиза

--- remake_pdfs.py
+++ remake_pdfs.py
@@ -4,13 +4,12 @@ from pdf2image import convert_from_path
 
 your_target_folder = "training_data"
 pdf_files = []
-for dirpath, _, filenames in os.walk(your_target_folder):
+for dir_path, _, filenames in os.walk(your_target_folder):
     for items in filenames:
-        file_full_path = os.path.abspath(os.path.join(dirpath, items))
+        file_full_path = os.path.abspath(os.path.join(dir_path, items))
         if file_full_path.lower().endswith(".pdf"):
             pdf_files.append(file_full_path)
 pdf_files.sort(key=str.lower)
-
 
 for file_path in pdf_files:
     writer = PdfFileWriter()
@@ -20,7 +19,7 @@ for file_path in pdf_files:
         right_page = reader.getPage(1)
     except:
         print("lol")
-    #page.scaleBy(0.1)
+    # page.scaleBy(0.1)
     writer.addPage(left_page)
     writer.addPage(right_page)
     with open(f"{file_path}", "wb") as output:
@@ -29,8 +28,8 @@ for file_path in pdf_files:
     image = convert_from_path(file_path)
     folder_name = file_path + "_folder"
     os.makedirs(folder_name)
-    number=1
+    number = 1
     for i in image:
         i.save(f"{folder_name}/{number}.jpg", "JPEG")
-        print(folder_name+" "+str(number)+" has been converted to jpeg")
-        number+=1
+        print(folder_name + " " + str(number) + " has been converted to jpeg")
+        number += 1
