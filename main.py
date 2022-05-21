7aaa2da9bf0a2ec8c4e4a8df1728140f1f51d7eb
Вадим <bigwhiterussianbatya@gmail.com>
2022-05-22 00:27:29 +0500

    image reading fix first word in paragraph skip fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from text_recognition import *
 from vectorize_text import *
 
-data_path = "D:\\Programming\\Python\\BookCoverRecognition\\training_data\\1. Русский язык коронавирусной эпохи, 2021.pdf_folder\\1.jpg"
+data_path = r"C:\coding\python\BookCoverRecognition\training_data\1__Русский_язык_коронавирусной_эпохи__2021\1.jpg"
 
 paragraphs = read_paragraphs_from_picture(data_path)
 
--- text_recognition.py
+++ text_recognition.py
@@ -1,26 +1,19 @@
-import cv2
+import os
+
 import pytesseract
+import matplotlib.pyplot as plt
 from pytesseract import Output
 
-pytesseract.pytesseract.tesseract_cmd = "D:\\Program Files\\Tesseract-OCR\\tesseract.exe"
-
-
-# работать мы будем с русскими доками, поэтому пока язык просто русский, потом сделаем чтобы можно было выбирать
-# скорее всего на этапе подготовления данных будем язык писать рядом в нужном формате
-def __read_text_from_file(path):
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    config = r'--oem 3 --psm 6'
-    return pytesseract.image_to_string(img, lang="rus", config=config)
+pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\\tesseract.exe"
 
 
 # возвращает массив со словарями, каждый содержит поля :
 # level, page_num, block_num, par_num, line_num, word_num, left,top, width, height, conf, text
 # содержимое полей понятно из названия, каждый словарь - 1 слово
 def read_paragraphs_from_picture(path):
-    img = cv2.imread(path)
-    read_paragraphs_from_picture_IMG(img)
+    img = plt.imread(path)
+    img = img[..., ::-1]
+    return read_paragraphs_from_picture_IMG(img)
 
 
 def __make_paragraphs(word_data_map, data):
@@ -42,15 +35,15 @@ def __make_paragraphs(word_data_map, data):
                                        "height": last_word["top"] - first_word["top"] + last_word["height"],
                                        "width": last_word["left"] - first_word["left"] + last_word["width"]}
             par_num = line["block_num"]
-            first_word = line
             text.clear()
+            text.append(line)
+            first_word = line
+
 
     return paragraphs
 
 
 def read_paragraphs_from_picture_IMG(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
     config = r'--oem 3 --psm 3'
     data = pytesseract.image_to_data(img, lang="rus", config=config, output_type=Output.DICT)
     word_data_map = []
@@ -74,4 +67,4 @@ def read_paragraphs_from_picture_IMG(img):
 
     result = __make_paragraphs(word_data_map, data)
 
-    return result.values()
+    return list(result.values())
