d025ba3cd9872d80461fa4fd150bca13a227863e
Geara0 <adrezz.26.5@gmail.com>
2022-05-22 23:15:16 +0500

    new hash func

--- vectorize_text.py
+++ vectorize_text.py
@@ -1,7 +1,25 @@
-def vectorize(text):
-    vectors = []
-    for i in text:
-        for j in i:
-            vectors.append(j)
+import re
 
-    return vectors
+dic = {}
+cnt = 0
+
+
+def vectorize(text):
+    data = []
+    for i in text:
+        if not i:
+            continue
+        string = re.sub("['} ]+|.*'text':", ' ', i['text']).strip().split(' ')
+        for i in string:
+            data.append(__hash(i))
+    return data
+
+
+def __hash(text):
+    global dic
+    global cnt
+    if text in dic:
+        return dic[text]
+    dic[text] = cnt
+    cnt += 1
+    return cnt - 1
