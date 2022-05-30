af9cbe4cc6043ca2dc612bec94d55253151bb7c5
Geara0 <adrezz.26.5@gmail.com>
2022-05-30 22:34:19 +0500

    finished

--- monography_parser.py
+++ monography_parser.py
@@ -14,48 +14,71 @@ def vectorize(text):
 
 def collective(text):
     isbn = set(re.findall(r"\s[\d\-–—]{13}\s|\s[\d\-–—]{17}\s", text))
-    authors = re.search(r"(?<=Авторы:).+?(?=[/(\r\n)])", text).group(0).strip()
-    name = re.search(r".+(?=колл)(?=.*моно)", text, re.IGNORECASE).group(0).strip()
-    pages = re.search(r"(?<= [\-–—] )\d{2,3}(?= с\.)", text).group(0).strip()
-    year = re.search(r"\d{4}(?=\.? [\-–—] )", text).group(0).strip()
-    publisher_and_other = re.search(rf"(?<=/).+?(?=, {year})", text).group(0).strip().split('/')
+    if len(isbn) != 0:
+        isbn = ' '.join([i.strip() for i in isbn])
+    else:
+        isbn = None
 
-    for i in isbn:
-        print(i.strip(), end=' ')
-    print()
-    print(authors)
-    print(name)
-    print(pages)
-    print(year)
-    print(publisher_and_other[len(publisher_and_other) - 1].strip())
+    authors = re.search(r"(?<=Авторы:).+?(?=[/(\r\n)])", text)
+    if authors:
+        authors = authors.group(0).strip()
+
+    name = re.search(r".+(?=колл)(?=.*моно)", text, re.IGNORECASE)
+    if name:
+        name = name.group(0).strip()
+
+    pages = re.search(r"(?<= [\-–—] )\d{2,3}(?= с\.)", text)
+    if pages:
+        pages = pages.group(0).strip()
+
+    year = re.search(r"\d{4}(?=\.? [\-–—] )", text)
+    publisher = None
+    if year:
+        year = year.group(0).strip()
+        publisher_and_other = re.search(rf"(?<=/).+?(?=, {year})", text)
+        if publisher_and_other:
+            publisher_and_other = publisher_and_other.group(0).strip().split('/')
+            publisher = publisher_and_other[len(publisher_and_other) - 1].strip()
+
+    return isbn, authors, name, pages, year, publisher
 
 
 def regular(text):
     isbn = set(re.findall(r"\s[\d\-—]{13}\s|\s[\d\-—]{17}\s", text))
-    authors = re.search(r"(?<=/ ).+(?= [\-–—])", text).group(0).strip()
-    name = re.search(rf"(?<=\d\d).*(?=/ {authors})", text, re.IGNORECASE).group(0).strip()
-    pages = re.search(r"(?<= [\-–—] )\d{2,3}(?= с\.)", text).group(0).strip()
-    year = re.search(r"\d{4}(?=\.? [\-–—] )", text).group(0).strip()
-    publisher_and_other = re.search(rf"(?<=/).+?(?=, {year})", text).group(0).strip().split('/')
-    for i in isbn:
-        print(i.strip(), end=' ')
-    print()
-    print(authors)
-    print(name)
-    print(pages)
-    print(year)
-    print(publisher_and_other[len(publisher_and_other) - 1].strip())
+    if isbn:
+        isbn = ' '.join([i.strip() for i in isbn])
+    authors = re.search(r"(?<=/ ).+(?= [\-–—])", text)
+    if authors:
+        authors = authors.group(0).strip()
+    name = re.search(rf"(?<=\d\d).*(?=/ {authors})", text, re.IGNORECASE)
+    if name:
+        name = name.group(0).strip()
+    pages = re.search(r"(?<= [\-–—] )\d{2,3}(?= с\.)", text)
+    if pages:
+        pages = pages.group(0).strip()
+    year = re.search(r"\d{4}(?=\.? [\-–—] )", text)
+    publisher = None
+    if year:
+        year = year.group(0).strip()
+        publisher_and_other = re.search(rf"(?<=/).+?(?=, {year})", text)
+        if publisher_and_other:
+            publisher_and_other = publisher_and_other.group(0).strip().split('/')
+            publisher = publisher_and_other[len(publisher_and_other) - 1].strip()
+
+    return isbn, authors, name, pages, year, publisher
 
 
 if __name__ == '__main__':
     # regular monography
-    # data_path = "D:\\Programming\\Python\\BookCoverRecognition\\training_data\\2__СМИ_как_медиатор_коммуникативно-культурной_памяти\\2.jpg"
+    data_path = "D:\\Programming\\Python\\BookCoverRecognition\\training_data\\1__Русский_язык_коронавирусной_эпохи__2021\\2.jpg"
     # collective monography
-    data_path = "D:\\Programming\\Python\\BookCoverRecognition\\training_data\\6__Травелог_Теоретико-методологический_анализ\\travelog.jpg"
+    # data_path = "D:\\Programming\\Python\\BookCoverRecognition\\training_data\\6__Травелог_Теоретико-методологический_анализ\\travelog.jpg"
 
     paragraphs = read_paragraphs_from_picture(data_path)
 
     dic = {}
     cnt = 0
     # regular(vectorize(paragraphs))
-    regular(vectorize(paragraphs))
+    regu = regular(vectorize(paragraphs))
+    coll = collective(vectorize(paragraphs))
+    0
