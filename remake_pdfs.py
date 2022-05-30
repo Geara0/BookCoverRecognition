d83b4f34021bacce4fb83e8ae0f5b8a83e5fca67
Вадим <bigwhiterussianbatya@gmail.com>
2022-05-31 01:49:18 +0500

    функция для конверта одного пдфа в две картинки

--- remake_pdfs.py
+++ remake_pdfs.py
@@ -1,4 +1,6 @@
 import os
+
+import pdf2image
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from pdf2image import convert_from_path
 
@@ -27,12 +29,23 @@ for file_path in pdf_files:
 
     image = convert_from_path(file_path)
     folder_name, file_ext = os.path.splitext(file_path)
-    folder_name= folder_name.replace(" ", "_")
-    folder_name= folder_name.replace(".", "_")
-    folder_name= folder_name.replace(",", "_")
+    folder_name = folder_name.replace(" ", "_")
+    folder_name = folder_name.replace(".", "_")
+    folder_name = folder_name.replace(",", "_")
     os.makedirs(folder_name)
     number = 1
     for i in image:
         i.save(f"{folder_name}/{number}.jpg", "JPEG")
         print(folder_name + "\\" + str(number) + " has been converted to jpeg")
         number += 1
+
+
+def convert_pdf_to_png(pdf_file, pages_to_convert=2):
+    pages = []
+    image = pdf2image.convert_from_bytes(pdf_file)
+    if len(image) < pages_to_convert:
+        raise Exception(f"Not enough pages to convert. Contains: {len(image)}, requested: {pages_to_convert}")
+    for i in range(pages_to_convert):
+        pages.append(image[i])
+
+    return pages
