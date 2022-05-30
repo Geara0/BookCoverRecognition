ffb304d3b06b71b2d73c9a9e784ad2d4f9e550db
Вадим <bigwhiterussianbatya@gmail.com>
2022-05-31 02:35:53 +0500

    добавил распознавание сборников и учебных пособий

--- main2.py
+++ main2.py
@@ -1,4 +1,7 @@
+import re
+
 import monography_parser
+import collection_and_tutor_parser
 from text_recognition import *
 
 
@@ -20,13 +23,23 @@ def join(a, b):
     return res
 
 
+def vectorize(source_text):
+    data = []
+    for i in source_text:
+        if not i:
+            continue
+        string = re.sub("['} ]+|.*'text':", ' ', i['text']).strip()
+        data.append(string)
+    return "\r\n".join(data)
+
+
 path = "D:\\Programming\\Python\\BookCoverRecognition\\training_data"
 answ = {}
 for _, dirs, _ in os.walk(path):
     for dir_ in dirs:
         try:
             text = read_paragraphs_from_picture(f"{path}\\{dir_}\\2.jpg")
-            text = monography_parser.vectorize(text)
+            text = vectorize(text)
             res = []
             if "монограф" in text:
                 res1 = monography_parser.collective(text)
@@ -34,6 +47,13 @@ for _, dirs, _ in os.walk(path):
                 # res = res2 if none_count(res1) <= none_count(res2) else res1
 
                 answ[dir_] = join(res1, res2)
+
+            if "сборник" in text:
+                res = collection_and_tutor_parser.collections(text)
+                answ[dir_] = res
+            if "пособие" in text or "лабораторный практикум" in text:
+                res = collection_and_tutor_parser.tutorial(text)
+                answ[dir_] = res
 
         except:
             print(f"{dir_}\\2.jpg")
